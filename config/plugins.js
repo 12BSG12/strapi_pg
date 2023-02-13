d3477ac724ab58740f87eba93a6b28d36c310f08
12BSG12 <carlj8335@gmail.com>
2023-02-14 01:11:16 +0500

    fix

--- config/plugins.js
+++ config/plugins.js
@@ -1,4 +1,18 @@
-module.exports = {
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
   "rest-cache": {
     config: {
       provider: {
@@ -17,4 +31,4 @@ module.exports = {
       },
     },
   },
-};
+});
